eb84f36df8fda3bf7ba9cda0417370c1c45c1f45
Long Phan <pvlong2000@gmail.com>
2021-06-18 18:08:23 +0400

    draw 9 points in 3x3 grid

--- sketch.js
+++ sketch.js
@@ -1,8 +1,27 @@
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(400, 400);
 }
 
+var points = [];
+var top_left_x = 100, top_left_y = 100;
+var cells_num = 3;
+var cell_width = 100;
+// populate points
+for (let i = 0; i < cells_num; i++){
+  for (let j = 0; j < cells_num; j++){
+    points.push([top_left_x + cell_width*i, top_left_y + cell_width*j]);
+  }
+}
+
+var point_size = 20;
+
+console.log(points);
+
 function draw() {
-  background(220);
-  circle(50, 50, 50);
+	background(220);
+	for (let i = 0; i < points.length; i++) {
+      let x = points[i][0];
+      let y = points[i][1];
+      ellipse(x,y, point_size, point_size);
+    }
 }
